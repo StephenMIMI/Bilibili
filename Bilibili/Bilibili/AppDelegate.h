f18e3009aaab8c5eb6cd1b724807f024c6a49b49
StephenMIMI <419886684@qq.com>
2016-11-23 10:08:02 +0800

    初始化项目

---
+++ Bilibili/Bilibili/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bilibili
+//
+//  Created by qianfeng on 16/11/23.
+//  Copyright © 2016年 zhb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Bilibili/Bilibili/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bilibili
+//
+//  Created by qianfeng on 16/11/23.
+//  Copyright © 2016年 zhb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
